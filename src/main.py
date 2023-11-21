194e4d605ea511a80144af10c4311aac7a8f8b5e
laiavg <laiavallesguilera@gmail.com>
2023-11-21 12:49:26 +0100

    Create upload endpoint and run splitter with files and params sent

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import os
+from typing import Annotated
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile, File, Form
+
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from src.db.models import Base
+from src.models import Parameters
 from src.splitter.splitter import split_text
 
 load_dotenv()
@@ -24,11 +27,23 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-def run_splitter(db: Session = Depends(get_db)):
-    split_text(db)
-    # Your FastAPI route logic here
-    return {"message": "Hello, FastAPI!"}
+@app.post("/upload")
+def run_splitter(
+        file: Annotated[UploadFile, File()],
+        chunk_size: int = Form(default=100),
+        chunk_overlap: int = Form(default=20),
+        is_separator_regex: bool = Form(default=False),
+        db: Session = Depends(get_db),
+):
+    parameters = Parameters(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        is_separator_regex=is_separator_regex
+    )
+
+    split_text(db, file, parameters)
+
+    return parameters
 
 
 if __name__ == "__main__":
--- src/splitter/splitter.py
+++ src/splitter/splitter.py
@@ -1,27 +1,25 @@
-import os
-
+from fastapi import UploadFile
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sqlalchemy.orm import Session
 
 from src.db.models import Document, Chunk
+from src.models import Parameters
+from src.utils import save_pdf
 
 
-def split_text(db: Session):
+def split_text(db: Session, file: UploadFile, parameters: Parameters):
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(current_directory, 'dummy.pdf')
-    loader = PyPDFLoader(filename)
+    path = save_pdf(file)
+    loader = PyPDFLoader(path)
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=100,
-        chunk_overlap=20,
         length_function=len,
-        is_separator_regex=False,
+        **vars(parameters)
     )
 
     pages = loader.load_and_split(text_splitter)
-    document = Document(filename=filename)
+    document = Document(filename=file.filename)
 
     for chunk in pages:
         db.add(Chunk(text=chunk.page_content, document=document))
