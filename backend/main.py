79a78ef927715847d2e855998c7614b212defb08
Laia Vallès <laia@sparsity-technologies.com>
2023-11-22 10:50:33 +0100

    Add endpoint to check tasks and flower dashboard

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import os
 from typing import Annotated
 
+from celery.result import AsyncResult
 from dotenv import load_dotenv
 from fastapi import FastAPI, Depends, UploadFile, File, Form, Body
 
@@ -62,10 +63,21 @@ def run_splitter(
 
 
 @app.post("/tasks", status_code=201)
-def run_task(payload = Body(...)):
+def run_task(payload=Body(...)):
     task_type = payload["type"]
     task = create_task.delay(int(task_type))
     return JSONResponse({"task_id": task.id})
+
+
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
 
 
 if __name__ == "__main__":
